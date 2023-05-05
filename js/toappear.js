18d2b8cf00035e4a222590e1634294a973e68b02
Vitorsantosdomingues <vitordossantosdomingues@gmail.com>
2023-05-05 13:01:48 -0300

    Atualizei o site

---
+++ js/toappear.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('#titulo-1', { duration: 1000});
+
+sr.reveal('.txt1', { duration: 1000});
+
+sr.reveal('.txt2', { duration: 1000});
+
+sr.reveal('#titulo-meunome', { duration: 1000});
+
