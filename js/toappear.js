709e06c8aa9e49dc38bd647b65fe7c4f78645e47
Vitorsantosdomingues <vitordossantosdomingues@gmail.com>
2023-05-18 14:04:59 -0300

    Atualizado

--- js/toappear.js
+++ js/toappear.js
@@ -1,8 +1,5 @@
 window.sr = ScrollReveal({ reset: true});
 
-sr.reveal('#subtittle', { duration: 1000});
-sr.reveal('#subtittle', { delay: 300});
-
 sr.reveal('#titulo-meunome', { duration: 1000});
 sr.reveal('#titulo-meunome', { delay: 300});
  
